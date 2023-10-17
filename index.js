375e680cfdf4f4c5d2f6d2b6cd160486c47062fe
Priyanshu-Nandi <anishkumar3787230@gmail.com>
2023-10-17 13:05:49 +0530

    first commit

--- index.js
+++ index.js
@@ -7,15 +7,16 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-
 io.on('connection',socket=>{
   socket.on('new-user-joined', name=>{
     users[socket.id]=name; 
-    socket.broadcast.emit('user-joined',name); 
+    socket.broadcast.emit('user-joined',name);
   });
+
   socket.on('send',message=>{
     socket.broadcast.emit('receive',{message:message,name:users[socket.id]})
 });
+
 socket.on('disconnect',message=>{
   socket.broadcast.emit('left',users[socket.id])
   delete users[socket.id];
